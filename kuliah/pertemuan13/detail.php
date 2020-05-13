cb0c9260e79b54e201c3407ecf817bb48bb7a924
Alief Fathurrohman <fathurrohmanalief@gmail.com>
2020-05-13 14:19:40 +0700

    menambahkan fitur ajax & upload gambar

---
+++ kuliah/pertemuan13/detail.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+  header("Location: login.php");
+  exit;
+}
+
+require 'functions.php';
+
+// jika tidak ada id url
+if (!isset($_GET['id'])) {
+  header("Location: index.php");
+  exit;
+}
+
+// ambil id dari url
+$id = $_GET['id'];
+// query mahasiswa berdasarkan id
+$m = query("SELECT * FROM mahasiswa WHERE id = $id");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Detail Mahasiswa</h3>
+  <ul>
+    <li><img width="250" src="img/<?= $m['gambar']; ?>"></li>
+    <li><?= $m['nrp']; ?></li>
+    <li><?= $m['nama']; ?></li>
+    <li><?= $m['email']; ?></li>
+    <li><?= $m['jurusan']; ?></li>
+    <li><a href="ubah.php?id=<?= $m['id']; ?>">ubah</a> | <a href="hapus.php?id=<?= $m['id']; ?> " onclick="return confirm('Hapus Data ???')">hapus</a></li>
+    <li><a href=" latihan3.php">Kembali ke daftar mahasiswa</a></li>
+
+  </ul>
+</body>
+
+</html>
